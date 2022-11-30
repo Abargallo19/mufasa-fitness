41fb4fc0be050621c8f879bbe81b992a5cf0782c
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-11-30 00:33:57 -0500

    format code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-
-
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Mufasa Fitness</h1>
       </header>
 
-<h3>Frontend operational!!</h3>
-
+      <h3>Frontend operational!!</h3>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-import App from './App';
+import App from "./App";
 import { BrowserRouter as Router } from "react-router";
 
 ReactDOM.render(
   <Router>
     <App />
-  </Router>
-  , document.getElementById("root"));
-
-
-
-
+  </Router>,
+  document.getElementById("root")
+);
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
